abc4b72928abf3ddf6687a2c24f885c083670db1
ewliusdxbl <82676823+ewliusdxbl@users.noreply.github.com>
2021-04-20 16:09:53 +0300

    added entities

---
+++ WebStoreExample.DAL/Entities/GoodsDal.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace WebStoreExample.DAL.Entities
+{
+    public class GoodsDal
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+        public CategoryDal Category { get; set; }
+
+        public double Price { get; set; }
+    }
+}
